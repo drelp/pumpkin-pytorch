a4ab0bcb73792a4a6998ec50c3e589733852b8a8
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-11-16 18:28:11 +0800

    1. add mnist train code

--- src/fashion_mnist/test.py
+++ src/fashion_mnist/test.py
@@ -23,6 +23,25 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
 if __name__ == "__main__":
     training_data = datasets.FashionMNIST(
         root="data",
@@ -55,3 +74,6 @@ if __name__ == "__main__":
 
     model = NeuralNetwork().to(device)
     print(model)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
