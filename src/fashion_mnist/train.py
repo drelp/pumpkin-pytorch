4911778c6614e83e8b3e98038f2f6cbcfd47f29d
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-11-16 21:40:26 +0800

    1. add mnist train code

--- src/fashion_mnist/train.py
+++ src/fashion_mnist/train.py
@@ -55,7 +55,7 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Train: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 if __name__ == "__main__":
